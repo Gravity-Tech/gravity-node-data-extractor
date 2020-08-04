8a7b55994fe24a509b9a0b3bdbf53148d17f7eaf
Shamil Khalilov <shamfama@gmail.com>
2020-08-04 21:35:31 +0300

    Rework

---
+++ tests/A_Waves_B_ETH_test.go
@@ -0,0 +1,17 @@
+package tests
+
+import (
+	"go/types"
+	"testing"
+)
+
+type testExtractionParameters struct {
+	MappedRequestIDList []string
+	CurrentBlock int
+	BlockInterestRange types.Tuple
+	Address string
+}
+func TestExtractionFromWavesToEth(t *testing.T) {
+
+
+}
